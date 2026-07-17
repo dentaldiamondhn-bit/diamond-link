5d5fa1a15a5f92f540ca898c3925052104c9e9ce
Diamond Link Developer <developer@diamond-link.com>
2026-07-16 19:36:59 -0600

    fix: remove border frame from consentimientos preview page
    Replace min-h-screen bg-gray-50 dark:bg-gray-900 py-8 wrapper
    with py-8 on the max-w-4xl container

--- app/(auth)/consentimientos/[id]/preview/page.tsx
+++ app/(auth)/consentimientos/[id]/preview/page.tsx
@@ -327,7 +327,7 @@ export default function ConsentimientoPreview() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-8">
+    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {/* Aggressive global print styles */}
       <style jsx global>{`
         @media print {
@@ -512,7 +512,6 @@ export default function ConsentimientoPreview() {
             day: 'numeric' 
           })}</p>
         </div>
-      </div>
     </div>
   );
 }
